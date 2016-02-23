d95f416e4bbeaf5911359e3e30d3643c80822fb9
Mestica <m.papna@gmail.com>
2016-02-23 08:14:55 +0800

    Hosted online

--- connection_local.php
+++ connection_local.php
@@ -1,5 +1,9 @@
 <?php
       $conn = mysql_connect("localhost","root","ask");
       $db = mysql_select_db("mdsf_student_information_system",$conn);
+
+//      $conn = mysql_connect("mysql1.000webhost.com","a6962825_mdsf","password");
+//      $db = mysql_select_db("a6962825_mdsf",$conn);
+
 ?>
 
